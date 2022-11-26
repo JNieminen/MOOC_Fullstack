3f85a513a773cd3ce94e9870f9a522219ce94fd8
Jarkko Nieminen <jarkko.nieminen@gmail.com>
2022-11-26 16:08:43 +0200

    Tehtava 2.10: puhelinluettelo step5

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,14 +2,46 @@ import { useState } from 'react'
 
 const Person = ({ person }) => (<>{person.name} {person.number}<br /></>)
 
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map(person =>
+      <Person key={person.name} person={person} />)
+    }
+  </div>
+)
+
+const Filter = ({ changeHandler, filter }) => (
+  <div>filter shown with <input value={filter}
+    onChange={changeHandler} />
+  </div>
+)
+
+const PersonForm = (props) => (
+  <form onSubmit={props.addName}>
+    <div>
+      name: <input value={props.newName}
+        onChange={props.handleNameChange} />
+    </div>
+    <div>
+      number: <input value={props.newNumber}
+        onChange={props.handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
 const App = () => {
   const debugData = [
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+    { name: 'Terry Gilliam', number: '44-01-798466' }
   ]
-  const [persons, setPersons] = useState(debugData)
+  const [persons, setPersons] = useState(debugData) // debug set
+  // const [persons, setPersons] = useState([]) // empty set
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -50,29 +82,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input value={filter}
-        onChange={handleFilterChange} />
-      </div>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName}
-            onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber}
-            onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {showPersons.map(person =>
-          <Person key={person.name} person={person} />)
-        }
-      </div>
-
+      <Filter changeHandler={handleFilterChange} filter={filter} />
+      <h3>Add a new</h3>
+      <PersonForm
+        addName={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={showPersons} />
     </div>
   )
 
