df19333c4a802614596a3369e52fa2ada14081c2
Jarkko Nieminen <jarkko.nieminen@gmail.com>
2022-11-26 12:16:43 +0200

    Tehtava 2.2

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,72 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const parts = props.course.parts
+  const total = parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0)
+
+  return (
+    <h4>total of {total} exercises</h4>
+  )
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </div>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App;
